905797a7b041f7c91b6c6967cfd4ddfc011aa35c
Torben Menke <59419684+entorb@users.noreply.github.com>
2021-11-13 19:42:30 +0100

    cleanup

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
+import datetime as dt
 import pandas as pd
 import helper
 
 
-df = pd.read_csv(
-    f'data/de-districts/de-district_timeseries-02000.tsv', sep="\t")
-df = helper.pandas_set_date_index(df)
+# df = pd.read_csv(
+#     f'data/de-districts/de-district_timeseries-02000.tsv', sep="\t")
+# df = helper.pandas_set_date_index(df)
 
-df = df["Cases_New"]
+# df = df["Cases_New"]
 
 
-print(df.tail())
+# print(df.tail())
