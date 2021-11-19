826747ba7af969071a17a7c8fdad9e6707c6b7e2
Torben Menke <59419684+entorb@users.noreply.github.com>
2021-11-19 18:21:07 +0100

    small fix

--- test.py
+++ test.py
@@ -16,3 +16,9 @@ import helper
 df_divi_latest = pd.read_csv(f"data/de-divi/downloaded/latest.csv", sep=",")
 
 print(df_divi_latest.tail())
+
+
+l1 = range(1, 16 + 1)
+l2 = ["de-states"] * len(l1)
+l3 = [df_divi_latest] * len(l1)
+print(list(zip(l1, l2, l3)))
