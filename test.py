57a9e5612778122c7b22cc462c6131f97fc307cd
Torben <59419684+entorb@users.noreply.github.com>
2022-01-29 10:21:51 +0100

    reorg

--- test.py
+++ test.py
@@ -1,33 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+This is my test playground and template for new scripts
+"""
+
+__author__ = "Dr. Torben Menke"
+__email__ = "https://entorb.net"
+__license__ = "GPL"
+
+# Built-in/Generic Imports
 import datetime as dt
+
+# Further Modules
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+# My Helper Functions
 import helper
 
+# Set German date format for plots: Okt instead of Oct
+import locale
 
-# df = pd.read_csv(
-#     f'data/de-districts/de-district_timeseries-02000.tsv', sep="\t")
-# df = helper.pandas_set_date_index(df)
-
-# df = df["Cases_New"]
-
-
-# print(df.tail())
-
-
-# df = pd.read_csv(f"data/de-divi/downloaded/latest.csv", sep=",")
-# idx = df.index[-1] + 1
-# print(idx)
-
-# df.loc[300000] = 123
-
-
-# print(df.tail())
-
-
-# l1 = range(1, 16 + 1)
-# l2 = ["de-states"] * len(l1)
-# l3 = [df_divi_latest] * len(l1)
-# print(list(zip(l1, l2, l3)))
-
-
-for row in range(16, 10 - 1, -1):
-    print(row)
+locale.setlocale(locale.LC_ALL, "de_DE.UTF-8")
